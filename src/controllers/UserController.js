3d07b25e8ba87ec70016c27b3fe164c5fdc4509d
Douglas Araujo <douglasaraujosampaio@gmail.com>
2021-09-24 11:46:02 -0300

    finished routes to Agendamentos

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -28,9 +28,10 @@ router.post('/registerUser', async (req, res) => {
 
 router.get('/listUser', async (req, res) => {
     try {
+        //console.log("oi")
         const usuarios = await User.find();
 
-        // console.log(usuarios[0])
+        //console.log(usuarios[0])
         return res.send({ usuarios });
 
     } catch (err) {
@@ -40,30 +41,30 @@ router.get('/listUser', async (req, res) => {
 
 router.put('/UpdateUser/:email', async (req, res) => {
     const { email } = req.params
-    
+
     try {
-        const user = await User.find({email})
+        const user = await User.find({ email })
         // console.log(email)
-        if(user.length==0){
+        if (user.length == 0) {
             return res.status(400).send({ error: 'failed' });
         }
-         
+
         const updateUser = await User.updateOne({ email }, {
             $set: {
                 name: req.body.name,
                 sexo: req.body.sexo,
                 telefone: req.body.telefone,
                 nacionalidade: req.body.nacionalidade,
-                endereco:{
-                    rua:req.body.rua,
-                    bairro:req.body.bairro,
-                    numero:req.body.numero,
-                    cep:req.body.cep
+                endereco: {
+                    rua: req.body.rua,
+                    bairro: req.body.bairro,
+                    numero: req.body.numero,
+                    cep: req.body.cep
                 }
             }
         })
 
-        
+
 
         // console.log(usuarios[0])
         return res.send({ updateUser });
@@ -74,9 +75,9 @@ router.put('/UpdateUser/:email', async (req, res) => {
 });
 
 router.delete('/DeleteUser/:email', async (req, res) => {
-    const {email} = req.params
+    const { email } = req.params
     try {
-        const deleteUser = await User.deleteOne({ email})
+        const deleteUser = await User.deleteOne({ email })
         if (!deleteUser.deletedCount) {
             return res.send({ erro: 'Usuario Não Encontrado' })
         }
@@ -122,12 +123,78 @@ router.post('/registerAgendamento/:email', async (req, res) => {
                 }
             }
         })
-        return res.status(200).send({ message:'Agendamento Cadastrado com sucesso' });
+        return res.status(200).send({ message: 'Agendamento Cadastrado com sucesso' });
 
 
     } catch (err) {
-         return res.status(400).send({ error: 'Registration failed' })
+        return res.status(400).send({ error: 'Registration failed' })
     }
 })
+
+router.get('/listAgendamentosByUser/:email', async (req, res) => {
+    const { email } = req.params
+
+    try {
+        const Agendamentos = await User.findOne({ email }, { Agendamento: 1 });
+        return res.send({ Agendamentos });
+
+    } catch (err) {
+        return res.status(400).send({ Agendamentos: [] });
+    }
+});
+
+router.put('/UpdateAgendamento/:email/:idAgendamento', async (req, res) => {
+    const { email, idAgendamento } = req.params
+    // console.log(email, idAgendamento)
+    
+    // console.log(user[0].Agendamento)
+    try {
+        const user = await User.find({ email, "Agendamento._id": idAgendamento })
+
+        if (user.length == 0) {
+            return res.status(400).send({ error: 'failed' });
+        }
+
+        const updateAgendamento = await User.updateOne({ email, "Agendamento._id": idAgendamento }, {
+            Agendamento: {
+                Data: req.body.Data,
+                Retorno: req.body.Retorno,
+                Descricao: req.body.Descricao,
+                Medico: req.body.Medico,
+
+            }
+        })
+
+
+
+        // console.log(usuarios[0])
+        return res.send({ updateAgendamento });
+
+    } catch (err) {
+        return res.status(400).send({ error: 'failed' });
+    }
+});
+router.delete('/DeleteAgendamento/:_id/:idAgendamento', async (req, res) => {
+    const { _id, idAgendamento } = req.params
+    try {
+        const deleteUser = await User.updateOne({
+            _id: _id
+        }, {
+            $pull: {
+                Agendamento: {
+                    _id: idAgendamento
+                }
+            }
+        })
+        if (!deleteUser.modifiedCount) {
+            return res.send({ erro: 'Agendamento Não Encontrado' })
+        }
+
+        return res.send({ Message: "Agendamento Removido com sucesso" });
+
+    } catch (err) {
+        return res.status(400).send({ error: 'delete failed' });
+    }
+});
 
 module.exports = app => app.use('/auth', router);
